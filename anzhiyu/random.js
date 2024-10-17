2bbd8985e645360754e7cb83d90eb3d9e361d995
liminstudio123 <ziyou8886@gmail.com>
2024-10-17 22:12:36 +0800

    Site updated: 2024-10-17 22:12:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/14/GitHub/","2024/10/14/hello-world/"];function toRandomPost(){
+var posts=["2024/10/17/利民资源合集（随时更新中）/","2024/10/14/hello-world/","2024/10/14/GitHub/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
