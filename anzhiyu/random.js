9831cf3ce63649ddab97e40abeb4ff6d874f2bc4
“liminstudio123” <ziyou8886@gmail.comgit config --global user.name liminstudio123git config --global user.email ziyou8886@gmail.com>
2024-10-18 23:39:39 +0800

    Site updated: 2024-10-18 23:39:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/18/利民资源合集（随时更新中）/","2024/10/18/节点搭建/","2024/10/18/网盘/","2024/10/18/图床/","2024/10/18/sink/"];function toRandomPost(){
+var posts=["2024/10/18/利民资源合集（随时更新中）/","2024/10/18/节点搭建/","2024/10/18/图床/","2024/10/18/网盘/","2024/10/18/sink/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
