2a78cfe80f70faf32d37ed11997dcab2066e7bae
liminstudio123 <ziyou8886@gmail.com>
2024-10-21 18:22:38 +0800

    Site updated: 2024-10-21 18:22:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/20/利民资源合集（随时更新中）/","2024/10/20/sink/","2024/10/20/hexoanzhiyu/","2024/10/20/图床/","2024/10/20/节点搭建/","2024/10/20/网盘/"];function toRandomPost(){
+var posts=["2024/10/20/利民资源合集（随时更新中）/","2024/10/20/hexoanzhiyu/","2024/10/20/sink/","2024/10/20/图床/","2024/10/20/网盘/","2024/10/20/节点搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
